81ed761093d8207f24a198049c3d23a09c99dd76
Mustofa12312 <cholilelgara001@gmail.com>
2026-08-28 22:12:40 +0700

    oke

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
 
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('products', ProductController::class);
